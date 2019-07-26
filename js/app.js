9d19e4b15cbe6156c2edbe4e33ae2cad48a612a0
kcbangi <kayzee@live.com>
2019-07-25 21:38:42 -1000

    console log json need to put them together

--- js/app.js
+++ js/app.js
@@ -1,33 +1,41 @@
 "use strict";
+
 const people = new XMLHttpRequest();
 people.addEventListener('load', function(){
     const personList = JSON.parse(this.responseText);
     const results = personList.results;
-    console.log(personList);
-
-    // for(let i = 0; i < result.length; i++) {
-    //     console.log(results);
-    // }
+    for(let i = 0; i < results.length; i++) {
+        let human = results[i];
+        console.log(`People${[i + 1]}: ${human.name}`);
+    }
 })
 people.open("GET", "https://swapi.co/api/people");
 people.send();
 
 
+const planets = new XMLHttpRequest();
+planets.addEventListener("load", function(){
+    const planetList = JSON.parse(this.responseText);
+    const results = planetList.results;
+    for(let i = 0; i < results.length; i++) {
+        let planet = results[i];
+        console.log(`Planet${[i + 1]}: ${planet.name}`);
+    }
+})
+planets.open("GET", "https://swapi.co/api/planets/");
+planets.send();
 
 
-// const planet = new XMLHttpRequest();
-// planet.addEventListener("load", function(){
-//     // const planetList = JSON.parse(this.responseText);
-//     // console.log(planetList);
-// })
-// people.open("GET", "https://swapi.co/api/planets/");
-// people.send();
+const starship = new XMLHttpRequest();
+starship.addEventListener("load", function() {
+    // console.log(JSON.parse(this.responseText));
+    const starshipList = JSON.parse(this.responseText);
+    const results = starshipList.results;
+    for(let i = 0; i < results.length; i++) {
+        let starships = results[i];
+        console.log(`Starship ${[i + 1]}: ${starships.name}`);
+    }
 
-
-
-// const starship = new XMLHttpRequest();
-// starship.addEventListener("load", function() {
-
-// })
-// starship.open("GET", "https://swapi.co/api/starships/");
-// starship.send();
+})
+starship.open("GET", "https://swapi.co/api/starships/");
+starship.send();
